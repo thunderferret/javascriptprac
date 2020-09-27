1ce6b867da30121d3d919a33a08e86019275429d
ThunderFerret <vpflt992@gmail.com>
2020-09-27 15:29:33 +0900

    #2.23 Join Controller
    Join Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,11 @@
-export const home = (req, res) => res.render("home");
+import {videos} from "../db";
+
+
+
+export const home = (req, res) =>{
+    res.render("home", {pageTitle : "Home",videos});
+};
+
 export const search = (req, res) => {
     const {
         query:{term}
@@ -6,9 +13,11 @@ export const search = (req, res) => {
     const searchingBy = req.query.term;
     res.render("search",{
         pageTitle : "Search",
-        searchingBy : searchingBy
+        searchingBy : searchingBy,
+        videos
     });
 }
+
 export const upload = (req, res) => res.render("upload");
 export const videoDetail = (req, res) => res.render("videoDetail");
 export const editVideo = (req, res) => res.render("editVideo");
---
+++ db.js
@@ -0,0 +1,51 @@
+export const videos = [
+{
+    id:324393,
+    title:`Video awesome`,
+    description:'This is something I love',
+    views:24,
+    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator:{
+        id:121212,
+        name: "Nicolas",
+        email:"NiCo@las.com"
+    }
+},
+{
+    id:324394,
+    title:`Video wow`,
+    description:'This is something I love',
+    views:24,
+    videoFile:"/media/cc0-videos/flower.webm",
+    creator:{
+        id:121212,
+        name: "Nicolas",
+        email:"NiCo@las.com"
+    }
+},
+{
+    id:324395,
+    title:`Video hancom`,
+    description:'This is something I love',
+    views:24,
+    videoFile:"/media/cc0-videos/flower.webm",
+    creator:{
+        id:121212,
+        name: "Nicolas",
+        email:"NiCo@las.com"
+    }
+},
+{
+    id:324396,
+    title:`Video awesome oracle`,
+    description:'This is something I love',
+    views:24,
+    videoFile:"/media/cc0-videos/flower.webm",
+    creator:{
+        id:121212,
+        name: "Nicolas",
+        email:"NiCo@las.com"
+    }
+}
+
+]

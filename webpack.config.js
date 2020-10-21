c3777f2122dc3f852de576a35c11d54dd3a9c49d
ThunderFerret <vpflt992@gmail.com>
2020-10-21 19:43:28 +0900

    scss update
    page scss update

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 
 const path = require("path");
+const { env } = require("process");
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets","js","main.js");
 const OUTPUT_DIR = path.join(__dirname,"static");
 
 const autopreFixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
-entry : ENTRY_FILE,
-plugins : [autopreFixer],
+entry : ["@babel/polyfill",ENTRY_FILE],
 module : {
 rules : [
     {
     test : /\.js$/,
     use:[
         'babel-loader'
-        
     ]
 }
     ,
@@ -23,13 +23,20 @@ rules : [
         test : /\.s[ac]ss$/i,
         use:[
             'style-loader',
+            MiniCssExtractPlugin.loader,
             'css-loader',
-            'sass-loader'
+         'sass-loader',
+    
+        
         ]
     }
 ]
 },
 mode : MODE,
+plugins : [autopreFixer,
+new MiniCssExtractPlugin({
+    filename:"styles.css"
+})],
 output : {
     path : OUTPUT_DIR,
     filename : "[name].js"
